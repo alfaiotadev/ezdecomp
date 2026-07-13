6006cb5ca7cdb5a1934c14722ddac4ddd15a433f
alfaiotadev <alfaiotadev@proton.me>
2026-07-14 01:59:38 +0300

    Implement NuFileGetEndianSwapOnWrite() (#63)
    Co-authored-by: alfaiotadev-bot <304523574+alfaiotadev@users.noreply.github.com>

--- src/nucore/NuFile.cpp
+++ src/nucore/NuFile.cpp
@@ -1,28 +1,12 @@
 #include "nucore/NuFile.h"
 
-int NuFile_SwapEndianOnWrite;
-int NuFile_SwapEndianOnRead;
+__attribute__((visibility("hidden"))) int* g_pNuFile_SwapEndianOnWrite;
+__attribute__((visibility("hidden"))) int* g_pNuFile_SwapEndianOnRead;
 
 int NuFileExtPlatformBit(int platform) {
     return 1 << platform;
 }
 
-int NuFileSwapEndianOnWrite(int value) {
-    int old = NuFile_SwapEndianOnWrite;
-    NuFile_SwapEndianOnWrite = value;
-    return old;
-}
-
-int NuFileSwapEndianOnRead(int value) {
-    int old = NuFile_SwapEndianOnRead;
-    NuFile_SwapEndianOnRead = value;
-    return old;
-}
-
-int NuFileGetEndianSwapOnRead() {
-    return NuFile_SwapEndianOnRead;
-}
-
 int NuFileGetEndianSwapOnWrite() {
-    return NuFile_SwapEndianOnWrite;
+    return *g_pNuFile_SwapEndianOnWrite;
 }
--- src/nucore/NuFile.h
+++ src/nucore/NuFile.h
@@ -1,7 +1,4 @@
 #pragma once
 
 int NuFileExtPlatformBit(int platform);
-int NuFileSwapEndianOnWrite(int value);
-int NuFileSwapEndianOnRead(int value);
-int NuFileGetEndianSwapOnRead();
 int NuFileGetEndianSwapOnWrite();
