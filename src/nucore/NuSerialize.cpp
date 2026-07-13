e8063641b1c7593fcc0e2aeb79f2d12e239c8758
alfaiotadev <alfaiotadev@proton.me>
2026-07-14 02:21:06 +0300

    Implement NuSerializeGetFieldSize(NuSerialize::FieldType) (#67)
    Co-authored-by: alfaiotadev-bot <304523574+alfaiotadev@users.noreply.github.com>

--- src/nucore/NuSerialize.cpp
+++ src/nucore/NuSerialize.cpp
@@ -4,6 +4,24 @@ class NuQuatBase;
 class NuVector3Base;
 class NuVector4Base;
 
+int NuSerialize::ms_fieldSizes[] = {
+    0, 1, 1, 1, 2, 2, 4, 4, 8, 8, 4, 1, 2, 4, 4, 16, 16, 16, 64, 4,
+    16, 16, 16, 64, 12, 16, 16, 64, 64, 4, 4, 4, 0, 0, 0, 0, 0, 16, 8, 16,
+    16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 4, 4, 8, 8,
+    4, 1, 2, 4, 4, 12, 16, 16, 64, 48, 0, 0, 0, 16,
+};
+
+static int* s_pNuSerializeFieldSizes = NuSerialize::ms_fieldSizes;
+
+int NuSerializeGetFieldSize(NuSerialize::FieldType type) {
+    return s_pNuSerializeFieldSizes[type];
+}
+
+template <>
+NuSerialize::FieldType NuSerializeGetFieldTypeMem<bool>() {
+    return static_cast<NuSerialize::FieldType>(0x1);
+}
+
 template <>
 NuSerialize::FieldType NuSerializeGetFieldTypeMem<float>() {
     return static_cast<NuSerialize::FieldType>(0xa);
--- src/nucore/NuSerialize.h
+++ src/nucore/NuSerialize.h
@@ -5,7 +5,11 @@ public:
     enum FieldType {
         kQuat = 0x16,
     };
+
+    static int ms_fieldSizes[];
 };
+
+int NuSerializeGetFieldSize(NuSerialize::FieldType type);
 
 template <typename T>
 NuSerialize::FieldType NuSerializeGetFieldTypeMem();
