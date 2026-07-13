ee7a1c83e0108d0063fffd240e168fe3d9d7839b
alfaiotadev <alfaiotadev@proton.me>
2026-07-14 01:50:36 +0300

    Implement NuStringTableGetBank() (#60)
    Co-authored-by: alfaiotadev-bot <304523574+alfaiotadev@users.noreply.github.com>

---
+++ src/nucore/NuStringTable.h
@@ -0,0 +1,20 @@
+#pragma once
+
+enum eStringBanks {
+    kStringBank_Story = 0,
+    kStringBank_FreePlay,
+    kStringBank_2,
+    kStringBank_3,
+    kStringBank_4,
+    kStringBank_5,
+    kStringBank_Count
+};
+
+struct NuStringTable {
+    int field0;
+    eStringBanks bank;
+    char pad[2872];
+    int format;
+};
+
+eStringBanks NuStringTableGetBank();
