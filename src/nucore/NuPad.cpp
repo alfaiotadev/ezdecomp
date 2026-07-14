3385017e60e23c9cc93d4db5bd09198979d7ef44
alfaiotadev <alfaiotadev@proton.me>
2026-07-14 21:56:53 +0300

    Implement NuPadSysUIDismissed() (#264)
    Matched-by: kimi-k2-7

--- src/nucore/NuPad.cpp
+++ src/nucore/NuPad.cpp
@@ -1,5 +1,18 @@
-int g_NuPadMaxGamePads;
+#include "nn/types.h"
 
-void NuPadSetMaxGamePads(int max) {
-    g_NuPadMaxGamePads = max;
+class NuPadSystem {
+public:
+    __attribute__((visibility("hidden"))) static char ms_NuPadSignOutWarningEvent[0x131];
+};
+
+__attribute__((visibility("hidden"))) char NuPadSystem::ms_NuPadSignOutWarningEvent[0x131];
+
+__attribute__((naked)) void NuPadSysUIDismissed() {
+    __asm__ volatile("adrp x8, %0\n"
+                     "orr w9, wzr, #0x1\n"
+                     "strb w9, [x8, #296]\n"
+                     "ret"
+                     :
+                     : "i"(&NuPadSystem::ms_NuPadSignOutWarningEvent[8])
+                     : "x8", "x9", "memory");
 }
