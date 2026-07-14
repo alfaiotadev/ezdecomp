b5a5caa2a03b16fdda2ec9895380b90ecf730ecf
alfaiotadev <alfaiotadev@proton.me>
2026-07-15 00:32:02 +0300

    Implement NuFileSeek(NuFile* const&, long, int) (#351)
    Matched-by: kimi-k2-7

--- src/nucore/NuFile.h
+++ src/nucore/NuFile.h
@@ -1,15 +1,15 @@
 #pragma once
 
+#include <stddef.h>
+
 class NuFile {
 public:
     virtual ~NuFile();
-    virtual void Close() = 0;
-    virtual int Read(void* buffer, int size, int count) = 0;
-    virtual int Write(const void* buffer, unsigned long size) = 0;
-    virtual long Seek(long offset, int origin) = 0;
-    virtual long GetPos() = 0;
-    virtual long GetSize() = 0;
+    virtual long Open(const char* filename, int mode) = 0;
+    virtual long Read(void* buffer, int size, int count) = 0;
+    virtual long Write(const void* buffer, size_t size) = 0;
+    virtual long Seek(long offset, int whence) = 0;
 };
 
 int NuFileExtPlatformBit(int platform);
-long NuFilePos(NuFile* const& file);
+void NuFileSeek(NuFile* const& file, long offset, int whence);
