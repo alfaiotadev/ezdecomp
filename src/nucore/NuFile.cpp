c36a062192aeb47c9b649c73d0106ba96da1b14b
alfaiotadev <alfaiotadev@proton.me>
2026-07-15 00:10:13 +0300

    Implement NuFileSwapEndianOnWrite(int) (#320)
    Matched-by: kimi-k2-7

--- src/nucore/NuFile.cpp
+++ src/nucore/NuFile.cpp
@@ -1,9 +1,13 @@
 #include "nucore/NuFile.h"
 
+extern int g_NuFileSwapEndianOnWrite;
+
 int NuFileExtPlatformBit(int platform) {
     return 1 << platform;
 }
 
-__attribute__((disable_tail_calls)) unsigned int NuFileTimeToTimeStamp(NuSystemTime time) {
-    return time.AsAndyDaviesTime();
+int NuFileSwapEndianOnWrite(int swap) {
+    int old = g_NuFileSwapEndianOnWrite;
+    g_NuFileSwapEndianOnWrite = swap;
+    return old;
 }
