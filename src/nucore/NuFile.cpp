168f49ccf2b709539ed00aef694aae92fa4f4e7e
alfaiotadev <alfaiotadev@proton.me>
2026-07-15 00:19:44 +0300

    Implement NuFileClose(NuFile* const&) (#348)
    Matched-by: kimi-k2-7

--- src/nucore/NuFile.cpp
+++ src/nucore/NuFile.cpp
@@ -1,34 +1,11 @@
 #include "nucore/NuFile.h"
 
-NuFile::~NuFile() {}
-
-void NuFile::Init() {}
-
-int NuFile::Read(void* buffer, int size, int count) {
-    return 0;
-}
-
-void NuFile::Write(const void* data, unsigned long size) {}
-
-long NuFile::Seek(long offset, int whence) {
-    return 0;
-}
-
-int NuFile::GetPos() {
-    return 0;
-}
-
-int NuFile::OpenSize() {
-    return 0;
-}
-
-int NuFileOpenSize(NuFile* const& file) {
-    if (file == 0) {
-        return 0;
-    }
-    return file->OpenSize();
-}
-
 int NuFileExtPlatformBit(int platform) {
     return 1 << platform;
+}
+
+NuFile::~NuFile() {}
+
+void NuFileClose(NuFile* const& file) {
+    delete file;
 }
