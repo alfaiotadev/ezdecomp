a38e2351e2ba211444e1a60314c738aca2984de0
alfaiotadev <alfaiotadev@proton.me>
2026-07-14 11:57:55 +0300

    Implement NuMovieGrabGetStartTimeCfg() (#97)
    Matched-by: kimi-k2-7

--- src/nu3d/NuMovieGrab.h
+++ src/nu3d/NuMovieGrab.h
@@ -1,3 +1,18 @@
-#pragma once
+#ifndef NU3D_NUMOVIEGRAB_H
+#define NU3D_NUMOVIEGRAB_H
 
-int NuMovieGrabGetEndFrameCfg(void);
+struct numoviegrabinfo_s {
+    char pad[20];
+    int startFrameTemp;
+    int startFrameCfg;
+    int endFrameTemp;
+    int endFrameCfg;
+    float startTimeTemp;
+    float startTimeCfg;
+    float endTimeTemp;
+    float endTimeCfg;
+};
+
+float NuMovieGrabGetStartTimeCfg();
+
+#endif
