45ce1e441d03604a6fd9b123284f84193ab65667
alfaiotadev <alfaiotadev@proton.me>
2026-07-14 13:52:46 +0300

    Implement NuGetCurrPlatformClass() (#144)
    Matched-by: kimi-k2-7

--- src/nucore/NuFile.cpp
+++ src/nucore/NuFile.cpp
@@ -1,17 +1,11 @@
 #include "nucore/NuFile.h"
 
-#include "nucore/NuString.h"
-
-__attribute__((visibility("hidden"))) char** g_pCurrentDirectoryBase;
+__attribute__((visibility("hidden"))) int* this_platformClass;
 
 int NuFileExtPlatformBit(int platform) {
     return 1 << platform;
 }
 
-char* NuFileGetCurrentDirectory() {
-    return *g_pCurrentDirectoryBase + 0x50;
-}
-
-void NuFileGetCurrentDirectory(char* dst) {
-    NuStrCpy(dst, *g_pCurrentDirectoryBase + 0x50);
+int NuGetCurrPlatformClass() {
+    return *this_platformClass;
 }
