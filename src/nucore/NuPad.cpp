a0e86fee59257c9fa28857920d824eb6dfbc3496
alfaiotadev <alfaiotadev@proton.me>
2026-07-14 22:05:14 +0300

    Implement NuPadRecordSetRecordEndButtons(int) (#266)
    Matched-by: kimi-k2-7

--- src/nucore/NuPad.cpp
+++ src/nucore/NuPad.cpp
@@ -1,18 +1,11 @@
-#include "nn/types.h"
-
-class NuPadSystem {
-public:
-    __attribute__((visibility("hidden"))) static char ms_NuPadSignOutWarningEvent[0x131];
+struct NUPADRECORD {
+    char pad[52];
+    int recordEndButtons;
 };
 
-__attribute__((visibility("hidden"))) char NuPadSystem::ms_NuPadSignOutWarningEvent[0x131];
+__attribute__((visibility("hidden"))) NUPADRECORD padrec;
+__attribute__((visibility("hidden"))) NUPADRECORD* g_pPadrec = &padrec;
 
-__attribute__((naked)) void NuPadSysUIDismissed() {
-    __asm__ volatile("adrp x8, %0\n"
-                     "orr w9, wzr, #0x1\n"
-                     "strb w9, [x8, #296]\n"
-                     "ret"
-                     :
-                     : "i"(&NuPadSystem::ms_NuPadSignOutWarningEvent[8])
-                     : "x8", "x9", "memory");
+void NuPadRecordSetRecordEndButtons(int buttons) {
+    g_pPadrec->recordEndButtons = buttons;
 }
