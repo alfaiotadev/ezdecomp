66f807276aafb9195822d99d5b0cc4bcc315af50
alfaiotadev <alfaiotadev@proton.me>
2026-07-15 00:15:43 +0300

    Implement NuFileOpenSize(NuFile* const&) (#322)
    Matched-by: kimi-k2-7

--- src/nucore/NuFile.h
+++ src/nucore/NuFile.h
@@ -1,4 +1,15 @@
 #pragma once
 
+class NuFile {
+public:
+    virtual ~NuFile();
+    virtual void Init();
+    virtual int Read(void* buffer, int size, int count);
+    virtual void Write(const void* data, unsigned long size);
+    virtual long Seek(long offset, int whence);
+    virtual int GetPos();
+    virtual int OpenSize();
+};
+
+int NuFileOpenSize(NuFile* const& file);
 int NuFileExtPlatformBit(int platform);
-int NuFileSwapEndianOnRead(int value);
