70a3d433c1762c5e5d4da03c05ca96157fa96219
alfaiotadev <alfaiotadev@proton.me>
2026-07-14 21:02:36 +0300

    Implement NuSetCutSceneCharacterDestroyDataFn(void (*)(NuCutSceneRaw*, NuANI4AnimRaw*, NuCutSceneRawCharExtra*, instNUGCUTCHAR_s*)) (#225)
    Matched-by: kimi-k2-7

--- src/nu2api/NuCutScene.cpp
+++ src/nu2api/NuCutScene.cpp
@@ -1,5 +1,14 @@
 #include "nu2api/NuCutScene.h"
 
-void NuSetCutSceneDestroyCharactersFn(void (*fn)(NuCutSceneRaw*)) {
-    NuCutSceneDestroyCharacters = fn;
+struct CutSceneCharacterDestroyDataCallbacks {
+    void (*fn)(NuCutSceneRaw*, NuANI4AnimRaw*, NuCutSceneRawCharExtra*, instNUGCUTCHAR_s*);
+};
+
+__attribute__((visibility("hidden")))
+CutSceneCharacterDestroyDataCallbacks* g_pCutSceneCharacterDestroyDataCallbacks;
+
+__attribute__((used)) void NuSetCutSceneCharacterDestroyDataFn(void (*fn)(NuCutSceneRaw*, NuANI4AnimRaw*,
+                                                                          NuCutSceneRawCharExtra*,
+                                                                          instNUGCUTCHAR_s*)) {
+    g_pCutSceneCharacterDestroyDataCallbacks->fn = fn;
 }
