d37ae93fe62647d28df48860ef666796e3217df3
alfaiotadev <alfaiotadev@proton.me>
2026-07-14 20:37:55 +0300

    Implement NuSetCutSceneCharacterEvalFn(void (*)(instNUGCUTSCENE*, NuCutSceneRaw*, instNUGCUTCHAR_s*, NuANI4AnimRaw*, float)) (#219)
    Matched-by: kimi-k2-7

---
+++ src/nu2api/CutSceneCharacterData.cpp
@@ -0,0 +1,7 @@
+struct instNUGCUTSCENE;
+struct NuCutSceneRaw;
+struct instNUGCUTCHAR_s;
+struct NuANI4AnimRaw;
+
+void (*g_pCutSceneCharacterEvalFn)(instNUGCUTSCENE*, NuCutSceneRaw*, instNUGCUTCHAR_s*, NuANI4AnimRaw*,
+                                   float);
