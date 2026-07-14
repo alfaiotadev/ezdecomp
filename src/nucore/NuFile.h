70e662e003cbda052df770197cb7ec25c5f3f134
alfaiotadev <alfaiotadev@proton.me>
2026-07-15 00:29:20 +0300

    Implement NuFilePos(NuFile* const&) (#350)
    Matched-by: kimi-k2-7

--- src/nucore/NuFile.h
+++ src/nucore/NuFile.h
@@ -1,4 +1,15 @@
 #pragma once
 
+class NuFile {
+public:
+    virtual ~NuFile();
+    virtual void Close() = 0;
+    virtual int Read(void* buffer, int size, int count) = 0;
+    virtual int Write(const void* buffer, unsigned long size) = 0;
+    virtual long Seek(long offset, int origin) = 0;
+    virtual long GetPos() = 0;
+    virtual long GetSize() = 0;
+};
+
 int NuFileExtPlatformBit(int platform);
-void NuFileCacheFile(const char* name);
+long NuFilePos(NuFile* const& file);
