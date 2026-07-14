664c711bc26ebcc5fec17255aa18da55228eee38
alfaiotadev <alfaiotadev@proton.me>
2026-07-15 00:56:57 +0300

    Implement NuRndrCurve(_vuv_s*, int, int, float, float, NuSmartPtr<NuMtl, true> const&, unsigned int, float, float) (#361)
    Matched-by: kimi-k2-7

--- src/nu3d/NuRender.h
+++ src/nu3d/NuRender.h
@@ -9,5 +9,12 @@ struct NUCOLOUR3 {
     float b;
 };
 
-void NuRndrSetParticleRotation(NUMTX* rotation);
+struct _vuv_s;
+class NuMtl;
+template <class T, bool b>
+class NuSmartPtr;
+
 void NuDrawBounds(const NuVector3Base& min, const NuVector3Base& max, NUMTX* mtx, NUCOLOUR3 const& colour);
+void NuRndrCurve2Clip(_vuv_s*, int, int, float, float, float, const NuSmartPtr<NuMtl, true>&, unsigned int,
+                      float, float, float);
+void NuRndrCurve(_vuv_s*, int, int, float, float, const NuSmartPtr<NuMtl, true>&, unsigned int, float, float);
