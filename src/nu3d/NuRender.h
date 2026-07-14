526a96ecabeed7df339fe55088064d80515aace2
alfaiotadev <alfaiotadev@proton.me>
2026-07-14 15:47:06 +0300

    Implement NuRndIsReflectionGobj() (#167)
    Matched-by: kimi-k2-7

---
+++ src/nu3d/NuRender.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "legoapi/BoxGeom.h"
+#include "numath/types.h"
+
+struct NUCOLOUR3 {
+    float r;
+    float g;
+    float b;
+};
+
+void NuDrawBounds(const NuVector3Base& min, const NuVector3Base& max, NUMTX* mtx, NUCOLOUR3 const& colour);
+int NuRndIsReflectionGobj();
